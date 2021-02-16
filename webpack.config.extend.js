c1f2998837cb1a69f6c80eacf192c964a0c3eb5e
mocmeo <vdev.lamnn@gmail.com>
2021-02-16 19:37:38 +0800

    UPD: Update cloudfront

--- webpack.config.extend.js
+++ webpack.config.extend.js
@@ -2,7 +2,7 @@ const linkingTarget = 'https://test.apple.com';
 
 // variables accessible in project src code
 const env = ['CDN'];
-process.env.CDN = '/cdn/assets';
+process.env.CDN = 'https://do2y4hg02hvd7.cloudfront.net/assets';
 
 module.exports = {
   linkingTarget,
