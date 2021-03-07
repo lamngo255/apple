d7de0f6d48f686d4bb47422970b17e8b5324b50b
meacc98 <ngo.baongoc9229@gmail.com>
2021-03-08 00:13:03 +0800

    UPD: Update favicon

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 const extendedConfig = require('./webpack.config.extend');
 
@@ -139,9 +139,10 @@ const config = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: './_redirects', to: "./"}
-      ]
-    })
+        { from: './_redirects', to: './' },
+        { from: './favicon.png', to: './' },
+      ],
+    }),
   ],
 
   devServer: {
